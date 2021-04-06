bcf25c75fed997ebd074cde018ade89312490029
sivapriyamurali <59977080+sivapriyamurali@users.noreply.github.com>
2021-04-05 21:16:09 -0400

    Update the SQL File with Other Tables

--- Extras/codejavadb.sql
+++ Extras/codejavadb.sql
@@ -28,3 +28,43 @@ CREATE TABLE `Transaction` (
   `Type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- ----------------------------
+-- Table structure for appointment
+-- ----------------------------
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE `appointment`  (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `Test Center` varchar(45) NOT NULL,
+  `Date` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Time` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Number of Slots` bigint(20) NOT NULL,
+  `Type` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of appointment
+-- ----------------------------
+INSERT INTO `appointment` VALUES (1, 'Madison Hts', '01/01/2021', '9:00 AM - 10:00 AM', 10,'Vaccine');
+INSERT INTO `appointment` VALUES (2, 'Madison Hts', '01/01/2021', '9:00 AM - 10:00 AM', 20,'Test');
+INSERT INTO `appointment` VALUES (3, 'Madison Hts', '01/01/2021', '9:00 AM - 10:00 AM', 30,'Vaccine');
+
+-- ----------------------------
+-- Table structure for test_center
+-- ----------------------------
+DROP TABLE IF EXISTS `test_center`;
+CREATE TABLE `test_center`  (
+  `center_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'testcenter id',
+  `name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'testcenter name',
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'testcenet address',
+  `zip_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`center_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of test_center
+-- ----------------------------
+INSERT INTO `test_center` VALUES (1, 'test_center1', 'address1', '4801');
+INSERT INTO `test_center` VALUES (2, 'test_center2', 'address2', '4802');

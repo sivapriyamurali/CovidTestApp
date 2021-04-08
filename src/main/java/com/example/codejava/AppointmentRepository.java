23906f1512eb14442338779d844b084c13e6544f
oliver0213 <34323353+oliver0213@users.noreply.github.com>
2021-04-08 16:27:38 -0400

    Delete src directory

--- src/main/java/com/example/codejava/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codejava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- src/main/java/com/example/codejava/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.codejava;
-
-import com.example.codejava.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}

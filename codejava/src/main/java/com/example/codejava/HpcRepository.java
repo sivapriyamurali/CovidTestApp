d41771ea8e9e85c9838b8fe581f51c60650befe4
oliver0213 <34323353+oliver0213@users.noreply.github.com>
2021-04-12 13:50:55 -0400

    Delete codejava directory

--- codejava/src/main/java/com/example/codejava/HpcRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.codejava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface HpcRepository extends JpaRepository<Hcp, Long> {
-    Hcp findByEmail(String email);
-
-}

e666bba128ef858919f1b85797c8f2cee4b69847
LiaoYouLe <LiaoYouLe@gmail.com>
2021-04-26 15:32:29 +0800

    fix

--- codejava/src/main/resources/templates/HC_PatientList.js
+++ codejava/src/main/resources/templates/HC_PatientList.js
@@ -1,8 +1,14 @@
 $(() => {
-
+    const now = new Date();
+    const nowStr = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
+    const dateNow = new Date(nowStr);
     const renderTb = (data) => {
         const html = data
             .filter(d => d.type === 'Test')
+            .filter(d=>{
+                const {date} = d
+                return dateNow >= new Date(`${date} 00:00:00`)
+            })
             .map(({user, testCenter, date, id, image}) => (`<tr>
                             <td>${user.fullname}</td>
                             <td>${testCenter}</td>
